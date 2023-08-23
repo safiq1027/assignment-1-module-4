44361eace5fa70a5252eccc203e564df9558786b
unknown <safiq1027@gmail.com>
2023-08-23 19:47:50 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref, reactive } from 'vue'
+
+const fullImage = ref('https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60')
+const images = reactive([
+  {
+    thumb: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+    isActive : 'active'
+  },
+  {
+    thumb: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+    isActive : 'not-active'
+  },
+  {
+    thumb: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60"
+  },
+  {
+    thumb: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=300&q=60",
+    full: "https://plus.unsplash.com/premium_photo-1666963323736-5ee1c16ef19d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60",
+    isActive : 'not-active'
+  }
+]);
+
+  const indexNo = ref(0)
+  
+  const forwardImage = ()=>{
+    if(indexNo.value < (images.length-1)){
+      indexNo.value++
+      images[indexNo.value].isActive = 'active'
+    }
+  }
+
+  const backwardImage = ()=>{
+    if(indexNo.value <= (images.length-1) && indexNo.value != 0){
+      images[indexNo.value].isActive = 'not-active'
+      indexNo.value--
+      images[indexNo.value].isActive = 'active'
+    }
+  };
+
+</script>
+
+<template>
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+
+    <div class="carousel-item" :class="image.isActive" v-for="(image, index) in images" :key="index">
+        <img class="d-block" style="width: 1300px;height:600px;" :src="image.full" :alt="image.full">
+    </div>
+
+  </div>
+
+  <a class="carousel-control-prev" href="javascript:void(0)" role="button" data-slide="prev" @click="backwardImage()">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+
+  <a class="carousel-control-next" href="javascript:void(0)" role="button" data-slide="next" @click="forwardImage()">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+
+</div>
+</template>
+
+<style scoped>
+
+</style>
